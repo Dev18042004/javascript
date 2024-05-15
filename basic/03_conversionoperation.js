c91374d2094b959e91a01841d071aa3139b6d281
Dev18042004 <pateldevmukeshbhai@gmail.com>
2024-05-15 08:31:41 +0000

    conversion

---
+++ basic/03_conversionoperation.js
@@ -0,0 +1,12 @@
+/*let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);*/
+
+let somenumber=33
+let Stringnumber=String(somenumber)
+console.log(typeof Stringnumber);
