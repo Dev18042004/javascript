95230710262de735898cce803c46b24c48b906d0
Dev18042004 <pateldevmukeshbhai@gmail.com>
2024-05-15 08:09:40 +0000

    datatype

---
+++ basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountid = 12233
+let accountemail ="dev1234@gmail.com"
+var accountpass = "1234"
+accountcity = "kadi"
+let accountstate;
+
+accountemail = "dcdc@gmail.com"
+accountpass = "4321"
+accountcity = "a'bad"
+console.log(accountid);
+console.log(accountemail);
+console.log(accountpass);
+console.log(accountcity);
+
+console.table([accountid,accountemail,accountpass,accountcity,accountstate]);
---
+++ basic/02_datatype.js
@@ -0,0 +1,10 @@
+"use strict";
+let name ="dev"
+let age =18
+let isLoggedIn = false
+let state = null
+console.log(typeof "dev")
+console.log(typeof age)
+console.log(typeof 18)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
